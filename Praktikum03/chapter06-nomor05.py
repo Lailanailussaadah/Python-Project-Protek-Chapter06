ac10abfee6d8d849aa8a510b0617dd387094c25f
Lailanailussaadah <71623025+Lailanailussaadah@users.noreply.github.com>
2020-11-05 03:43:03 +0800

    Create chapter06-nomor05.py

---
+++ Praktikum03/chapter06-nomor05.py
@@ -0,0 +1,21 @@
+from operation import*
+
+#a
+a = kali (4, 6)
+b = jumlah (2, a)
+c = kurang (b, 4)
+print(c)
+
+#b
+a = jumlah (4, 7)
+b = kurang (6, 9)
+c = kali (a, b)
+print (c)
+
+#c
+a = jumlah (10, 2)
+b = jumlah (7, 5)
+c = kurang (12, 34)
+d = bagi (a, b)
+e = bagi (d, c)
+print (e)
